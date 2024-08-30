38fc2d87b507676a30b6cec977ba5e432d4bcfb8
Talisson Pontes Rodrigues <Talisson53899806@edu.df.senac.br>
2024-08-30 08:48:54 -0300

    Modelo SQL

---
+++ documentacao/ProjetoIntegradorTelemedicina-SQL.sql
@@ -0,0 +1,97 @@
+CREATE TABLE Paciente 
+( 
+ ID INT PRIMARY KEY AUTO_INCREMENT,  
+ nome VARCHAR(100),  
+ cpf VARCHAR(15),  
+ dataNascimento DATE,  
+ genero VARCHAR(9),  
+ telefone VARCHAR(15),  
+ endereco VARCHAR(200),  
+ email VARCHAR(100),  
+ rg VARCHAR(16)
+); 
+
+CREATE TABLE Medico 
+( 
+ ID INT PRIMARY KEY AUTO_INCREMENT,  
+ nomeMed VARCHAR(100),  
+ cpfMed VARCHAR(15),  
+ nascimentoMed DATE,  
+ generoMed VARCHAR(9),  
+ telefoneMed VARCHAR(15),  
+ enderecoMed VARCHAR(200),  
+ emailMed VARCHAR(100),  
+ rgMed VARCHAR(16),  
+ registroMed VARCHAR(12),  
+ especializacao VARCHAR(100),  
+ instituicaoEnsino VARCHAR(100),  
+ conclusaoGrad DATE,  
+ certificacao VARCHAR(100),  
+ areaInteresse VARCHAR(100),  
+ clinicaTrabalho VARCHAR(100),  
+ credenciais VARCHAR(100),  
+ experiencias VARCHAR(1500),  
+ ID_CONSULTA INT  
+); 
+
+CREATE TABLE Consulta 
+( 
+ ID INT PRIMARY KEY AUTO_INCREMENT,  
+ dataConsulta DATE,  
+ formatoConsulta VARCHAR(10),  
+ ID_PACIENTE INT,  
+ ID_MEDICO INT,  
+ ID_CLINICA INT,    
+ pagamentoConsulta FLOAT,  
+ areaProcura VARCHAR(200)  
+); 
+
+CREATE TABLE Clinica 
+( 
+ ID INT PRIMARY KEY AUTO_INCREMENT,  
+ nomeClinica VARCHAR(100),  
+ enderecoClinica VARCHAR(200),  
+ cnpjClinica VARCHAR(18),  
+ telefoneClinica VARCHAR(15),  
+ emailClinica VARCHAR(100)  
+); 
+
+CREATE TABLE Exame 
+( 
+ ID INT PRIMARY KEY AUTO_INCREMENT,  
+ diagnosticoExame VARCHAR(300),  
+ statusExame VARCHAR(12),  
+ ID_PACIENTE INT,  
+ ID_MEDICO INT,  
+ ID_CLINICA INT,  
+ dataExame DATE,  
+ localExame VARCHAR(200),  
+ descricaoExame VARCHAR(300)  
+); 
+
+CREATE TABLE Prescricao 
+( 
+ assinatura VARCHAR(100),  
+ medicamento VARCHAR(300),  
+ ID INT PRIMARY KEY AUTO_INCREMENT,  
+ ID_PACIENTE INT,  
+ ID_MEDICO INT  
+); 
+
+CREATE TABLE Atende 
+( 
+ ID_MEDICO INT,  
+ ID_CLINICA INT  
+); 
+
+ALTER TABLE Medico ADD FOREIGN KEY(ID_CONSULTA) REFERENCES Consulta (ID);
+ALTER TABLE Consulta ADD FOREIGN KEY(ID_PACIENTE) REFERENCES Paciente (ID);
+ALTER TABLE Consulta ADD FOREIGN KEY(ID_MEDICO) REFERENCES Medico (ID);
+ALTER TABLE Consulta ADD FOREIGN KEY(ID_CLINICA) REFERENCES Clinica (ID);
+ALTER TABLE Exame ADD FOREIGN KEY(ID_PACIENTE) REFERENCES Paciente (ID);
+ALTER TABLE Exame ADD FOREIGN KEY(ID_MEDICO) REFERENCES Medico (ID);
+ALTER TABLE Exame ADD FOREIGN KEY(ID_CLINICA) REFERENCES Clinica (ID);
+ALTER TABLE Prescricao ADD FOREIGN KEY(ID_PACIENTE) REFERENCES Paciente (ID);
+ALTER TABLE Prescricao ADD FOREIGN KEY(ID_MEDICO) REFERENCES Medico (ID);
+ALTER TABLE Atende ADD FOREIGN KEY(ID_MEDICO) REFERENCES Medico (ID);
+ALTER TABLE Atende ADD FOREIGN KEY(ID_CLINICA) REFERENCES Clinica (ID);
