7bad48ad5800664e28d5d8401addd5cf4928a3ed
Talisson Pontes Rodrigues <Talisson53899806@edu.df.senac.br>
2024-09-02 09:18:44 -0300

    Atualização SQL

---
+++ banco_dados/consulta_insert.sql
@@ -0,0 +1,25 @@
+use projetoIntegradorTelemedicina;
+
+INSERT INTO Consulta 
+(dataConsulta, formatoConsulta, pagamentoConsulta, areaProcura)
+VALUES 
+('2024-08-01', 'Presencial', 250.00, 'Cardiologia Preventiva'),
+('2024-08-02', 'Online', 200.00, 'Dermatologia Estética'),
+('2024-08-03', 'Presencial', 300.00, 'Gastroenterologia Clínica'),
+('2024-08-04', 'Online', 180.00, 'Ginecologia Obstétrica'),
+('2024-08-05', 'Presencial', 270.00, 'Ortopedia e Traumatologia'),
+('2024-08-06', 'Presencial', 220.00, 'Endocrinologia Reprodutiva'),
+('2024-08-07', 'Online', 150.00, 'Psiquiatria Infantil'),
+('2024-08-08', 'Presencial', 320.00, 'Oftalmologia Cirúrgica'),
+('2024-08-09', 'Online', 190.00, 'Nefrologia Clínica'),
+('2024-08-10', 'Presencial', 250.00, 'Cirurgia Plástica Reconstrutiva'),
+('2024-08-21', 'Presencial', 240.00, 'Otorrinolaringologia'),
+('2024-08-22', 'Online', 210.00, 'Reumatologia'),
+('2024-08-23', 'Presencial', 275.00, 'Cirurgia Geral'),
+('2024-08-24', 'Online', 230.00, 'Urologia'),
+('2024-08-25', 'Presencial', 250.00, 'Hematologia'),
+('2024-08-26', 'Online', 195.00, 'Gastroenterologia Pediátrica'),
+('2024-08-27', 'Presencial', 260.00, 'Infectologia'),
+('2024-08-28', 'Online', 220.00, 'Pneumologia'),
+('2024-08-29', 'Presencial', 280.00, 'Endocrinologia Infantil'),
+('2024-08-30', 'Online', 185.00, 'Medicina de Família e Comunidade'),
