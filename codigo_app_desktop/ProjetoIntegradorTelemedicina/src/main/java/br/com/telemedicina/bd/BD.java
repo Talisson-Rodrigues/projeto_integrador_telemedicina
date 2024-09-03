980fb822f7068ce22eea8988bbd2730acd0c16bb
Talisson Pontes Rodrigues <Talisson53899806@edu.df.senac.br>
2024-09-03 08:19:14 -0300

    Atualização banco de dados

--- codigo_app_desktop/ProjetoIntegradorTelemedicina/src/main/java/br/com/telemedicina/bd/BD.java
+++ codigo_app_desktop/ProjetoIntegradorTelemedicina/src/main/java/br/com/telemedicina/bd/BD.java
@@ -5,9 +5,12 @@
 package br.com.telemedicina.bd;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 public class BD {
-    private String url = "jdbc:mysql://10.160.222.12:3307/projetoIntegradorTelemedicina";
+    private String url = "jdbc:mysql://localhost:3307/projetoIntegradorTelemedicina";
     private String user = "root";
     private String password = "senac";
     private Connection conn;
@@ -16,6 +19,31 @@ public class BD {
         try {
             //Carrega o driver
             Class.forName("com.mysql.cj.jdbc.Driver");
+            // Estabelecer a conexão
+            this.conn = DriverManager.getConnection(url, user, password);
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+            return false;
         }
+        return true;
+    }
+    
+    public boolean encerrarConexao() {
+        try {
+           this.conn.close(); 
+        } catch (SQLException e) {
+           e.printStackTrace();
+           return false;
+        }
+        return true;
+    }
+    
+    public PreparedStatement getPreparedStatement(String query) {
+        try {
+            return this.conn.prepareStatement(query);
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+        return null;
     }
 }
