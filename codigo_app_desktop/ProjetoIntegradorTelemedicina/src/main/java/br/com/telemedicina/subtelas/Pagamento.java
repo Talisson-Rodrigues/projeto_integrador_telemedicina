3c6901d02f6066cb3e8f327822f1b82ea03da902
Talisson Pontes Rodrigues <Talisson53899806@edu.df.senac.br>
2024-09-23 08:47:37 -0300

    Atualização

--- codigo_app_desktop/ProjetoIntegradorTelemedicina/src/main/java/br/com/telemedicina/subtelas/Pagamento.java
+++ codigo_app_desktop/ProjetoIntegradorTelemedicina/src/main/java/br/com/telemedicina/subtelas/Pagamento.java
@@ -375,6 +375,12 @@ public class Pagamento extends javax.swing.JDialog {
         }
         //</editor-fold>
         //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
 
         /* Create and display the dialog */
         java.awt.EventQueue.invokeLater(new Runnable() {
